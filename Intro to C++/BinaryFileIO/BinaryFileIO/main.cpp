5b1170fe1c3e391649ea2a2c1e38c707baf0f106
Josh Moten <josh.moten@hotmail.com>
2019-03-29 13:20:38 +1100

    29/3/2019 - Finished Binary FIle IO
    Intro to C++

--- Intro to C++/BinaryFileIO/BinaryFileIO/main.cpp
+++ Intro to C++/BinaryFileIO/BinaryFileIO/main.cpp
@@ -1,37 +1,107 @@
 #include <iostream>
 #include <fstream>
 
-using namespace std;
 
-const int BUFFER_SIZE = 12;
+int main()
+{
+	std::fstream movie_data;
 
-int main() {
-	char characterArray[BUFFER_SIZE] = { 'H', 'e', 'l', 'l', 'o', ' ', 't', 'h', 'e', 'r', 'e', '!'};
+	//Writing to text file
+	movie_data.open("..\\movie_data.txt", std::ios_base::out);
+	if (movie_data.is_open())
+	{
+		movie_data << "Movie Name: The Good, The Bad, The Ugly\n";
+		movie_data << "Release Year: 1966\n";
+		movie_data << "Director: Sergio Leone\n";
+		movie_data << "Rotten Tomatoes Score: 97%\n";
+		movie_data.close();
+	}
+	else
+	{
+		std::cout << "Write failed" << std::endl;
+	}
 
-	char *character;
+	//Reading from a text file
+	char buffer[200];
+	movie_data.open("..\\movie_data.txt", std::ios_base::in);
+	if (movie_data.is_open())
+	{
+		std::cout << "*** Text File Full Read: ***" << std::endl;
+		while (!movie_data.eof())
+		{
+			movie_data >> buffer;
+			std::cout << buffer << " ";
+		}
+		movie_data.close();
+		std::cout << "\n\n";
+	}
+	else
+	{
+		std::cout << "Read failed\n";
+	}
 
-	ofstream fout("Data.dat", ios_base::out | ios::binary);
-	fout.seekp(0, ios::beg);
-	fout.write(characterArray, BUFFER_SIZE);
 
-	fout.close();
+	//Writing to a binary file
+	const char* text = "Movie Name: The Good, The Bad, The Ugly\nRelease Year: 1966\nDirector: Sergio Leone\nRotten Tomatoes Score: 97%\n";
+	movie_data.open("..\\movie_data.dat", std::ios_base::out | std::ios::binary);
+	if (movie_data.is_open())
+	{
+		movie_data.write(text, sizeof(char) * strlen(text) + 1);
+		movie_data.close();
+	}
+	else
+	{
+		std::cout << "Binary write failed\n";
+	}
 
-	cout << "==\tFile Written!\t==" << endl << endl;
+	//Reading from a binary file
+	char buffer2[200];
+	movie_data.open("..\\movie_data.dat", std::ios_base::in | std::ios_base::binary);
+	if (movie_data.is_open())
+	{
+		movie_data.read(buffer2, sizeof(char) * strlen(buffer2) + 1);
+		std::cout << "*** Binary File Full Read: ***\n";
+		std::cout << buffer2;
+		movie_data.close();
+		std::cout << "\n\n";
+	}
+	else
+	{
+		std::cout << "Binary read failed\n";
+	}
 
 
+	//Writing to the middle of a binary file
+	movie_data.open("..\\movie_data.dat", std::ios_base::out | std::ios::binary);
+	if (movie_data.is_open())
+	{
+		movie_data.seekp(12);
+		movie_data.write("PTING", 6);
+		movie_data.close();
+	}
+	else
+	{
+		std::cout << "Binary middle write failed\n";
+	}
 
-	ifstream fin("Data.dat", ios_base::in || ios::binary || ios::ate);
+	//Reading from the middle of a binary file
+	char buffer3[6];
+	movie_data.open("..\\movie_data.dat", std::ios_base::in | std::ios_base::binary);
+	if (movie_data.is_open())
+	{
+		movie_data.seekg(12);
+		movie_data.read(buffer3, 6);
+		std::cout << "*** Binary File Middle Read: ***\n";
+		std::cout << buffer3;
+		movie_data.close();
+		std::cout << "\n\n\n";
+	}
+	else
+	{
+		std::cout << "Binary middle read failed\n";
+	}
 
-	streampos size = fin.tellg();
-	character = new char[size];
-	fin.seekg(0, ios::beg);
-	//fin.seekg(5, ios::cur);
-	fin.read(character, BUFFER_SIZE);
-	cout << character;
-
-	cout << endl;
-
-	fin.close();
+	std::cout << "\nDidn't have time to polish it in b/w doctors appointments and work\nBut this does what the criteria requires.\nCheers!\n\n";
 
 	system("pause");
 }
